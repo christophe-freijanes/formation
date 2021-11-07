3f8107603d702b411d477b92bd3b34266ed93442
christophe.freijanes <cfreijanes@gmx.fr>
2021-11-07 01:09:15 +0100

    Add miniprojet

---
+++ DevOps/docker/miniprojet/VM/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostsupdater",["= 1.2.4"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"1e374b72231a84ef49fbe6427dac439cc90e4bb1023e83ec4072e101c49cd4c4","vagrant_version":"2.2.18"}
