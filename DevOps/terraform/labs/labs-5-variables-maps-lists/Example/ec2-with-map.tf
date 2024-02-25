352c1d41fcb8033c518020c3de7db24aed9c8d6c
⚡Christophe FREIJANES <cfreijanes@gmx.fr>
2024-02-25 18:37:20 -0500

    Update terraform Folder
    Signed-off-by: ⚡Christophe FREIJANES <cfreijanes@gmx.fr>

--- DevOps/terraform/labs/labs-5-variables-maps-lists/Example/ec2-with-map.tf
+++ DevOps/terraform/labs/labs-5-variables-maps-lists/Example/ec2-with-map.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-012cc038cc685a0d7"
   # On va exploiter dans notre liste une machine de type "m5.large"
-  instance_type = var.types['us-east-1']
+  instance_type = var.types["us-east-1"]
 }
 
 # Definition d'une liste de type de machine
@@ -23,6 +23,6 @@ variable "types" {
   default = {
     us-east-1 = "t2.micro"
 	us-west-2 = "t2.nano"
-	ap-south-1 = "t2.small"`
+	ap-south-1 = "t2.small"
   }
 }
